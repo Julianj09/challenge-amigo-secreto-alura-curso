3e64c1dcd5809c01dc9b35f0074be0adb5010592
Julianj09 <didierjuliantabacoduran@gmail.com>
2025-03-16 19:05:21 -0500

    Agregada la funcion sortearAmigo

--- app.js
+++ app.js
@@ -24,3 +24,20 @@ function actualizarLista() {
         lista.appendChild(li);
     });
 }
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.querySelector("#resultado");
+    resultado.innerHTML = "";
+
+    let li = document.createElement("li");
+    li.textContent = `El amigo secreto es: ${amigoSorteado}`;
+    resultado.appendChild(li);
+}
